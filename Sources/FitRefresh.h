3813901aae56d7569289f6850ad077666d1065e7
Cyrill <lichunyang@outlook.com>
2017-04-28 17:08:02 +0800

    add info plist
    add FitHeader.h

---
+++ Sources/FitRefresh.h
@@ -0,0 +1,19 @@
+//
+//  Component.swift
+//  FitRefresh
+//
+//  Created by Cyrill on 2016/12/28.
+//  Copyright © 2016年 Cyrill. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FitRefresh.
+FOUNDATION_EXPORT double FitRefreshVersionNumber;
+
+//! Project version string for FitRefresh.
+FOUNDATION_EXPORT const unsigned char FitRefreshVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FitRefresh/PublicHeader.h>
+
+
