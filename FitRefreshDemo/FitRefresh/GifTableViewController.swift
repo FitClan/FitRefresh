f2cbbb71855641a48589c9d5847a6011f9a584ec
cywd <sakura_29@163.com>
2018-02-02 11:30:50 +0800

    update demo code

--- FitRefreshDemo/FitRefresh/GifTableViewController.swift
+++ FitRefreshDemo/FitRefresh/GifTableViewController.swift
@@ -64,7 +64,6 @@ class GifTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return dataArray.count
     }
-
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "FRGifCellID", for: indexPath)
